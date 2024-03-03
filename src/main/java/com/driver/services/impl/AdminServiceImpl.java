109c4b75c967251a70a05bfa1d6525108e40ac1a
HP <divyashreeharitha@gmail.com>
2024-03-03 19:24:09 +0530

    commit 1

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,5 +1,9 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
@@ -20,13 +24,39 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
+        Admin admin = new Admin(username,password);
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+        ServiceProvider serviceProvider = new ServiceProvider();
+        Admin admin = adminRepository1.findById(adminId).orElse(null);
+        serviceProvider.setAdmin(admin);
+        serviceProvider.setName(providerName);
+        serviceProviderRepository1.save(serviceProvider);
+        return admin;
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+        String countryNamUpper = countryName.toUpperCase();
+        boolean isPresent = false;
+        for(CountryName countryName1 : CountryName.values()){
+            if(countryName1.toString().equals(countryNamUpper)){
+                isPresent = true;
+                break;
+            }
+        }
+        if(!isPresent) throw new Exception("Country name not found");
+        ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+        Country country = new Country();
+        country.setCountryName(CountryName.valueOf(countryNamUpper));
+        country.setCode(CountryName.valueOf(countryNamUpper).toCode());
+        country.setServiceProvider(serviceProvider);
+        serviceProvider.getCountryList().add(country);
+        serviceProviderRepository1.save(serviceProvider);
+        return serviceProvider;
     }
 }
